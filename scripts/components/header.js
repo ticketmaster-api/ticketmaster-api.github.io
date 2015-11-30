f3ccdd0f2d0f86dc2258c3cec1305a154e8cdf9d
Vitalii_Menshutin <Vitalii_Menshutin@epam.com>
2015-11-30 13:39:55 +0200

    ref: Header javascript was refactored

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -1,23 +1,40 @@
 (function(){
+
     $(document).ready(function(){
 
-        var menuDropdown = $('#menu-dropdown'),
-            menuBtn = $('#menu-btn')
+        var Header = {
+            test: true,
+            menuDropdown: $('#menu-dropdown'),
+            menuBtn: $('#menu-btn'),
+            show: function(){
+                var self = this;
+                if (!self.menuBtn.hasClass('tm-close')){
+                    self.menuBtn.addClass('tm-close');
+                    self.menuDropdown.removeClass('closed').addClass('expanded');
+                    self.menuDropdown.focus();
+                }
+            },
+            hide: function(){
+                var self = this;
+                setTimeout(function(){
+                    self.menuBtn.removeClass('tm-close')
+                }, 300);
+                self.menuDropdown.removeClass('expanded').addClass('closed');
+            },
+            init: function(){
+                var self = this;
+                self.menuBtn.on("click", function(){
+                    self.show();
+                });
 
-        menuBtn.on("click", function(){
-            if (!menuBtn.hasClass('tm-close')){
-                menuBtn.addClass('tm-close');
-                menuDropdown.removeClass('closed').addClass('expanded');
-                menuDropdown.focus();
+                this.menuDropdown.on('blur', function(){
+                    self.hide();
+                });
             }
-        });
+        };
 
-        menuDropdown.on('blur', function(){
-            setTimeout(function(){
-                menuBtn.removeClass('tm-close')
-            }, 300);
-            menuDropdown.removeClass('expanded').addClass('closed');
-        });
-    })
+        Header.init();
+
+    });
 
 })();
