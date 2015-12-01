9fb52df43a4ec2269f80424a7aac2f8121e65364
RaNKaR <esc.puf@gmail.com>
2015-12-01 15:21:27 +0200

    [ref]: removed console logs

--- scripts/components/pd-side-menu.js
+++ scripts/components/pd-side-menu.js
@@ -3,10 +3,7 @@
         var secondaryNav = $('#sideMenu');
         var offset = 307;
 
-        console.log([$('#sideMenu').offset().top,offset]);
-
         $(window).on('scroll', function () {
-            console.log($(window).scrollTop());
             if ($(window).scrollTop() > offset) {
                 secondaryNav.addClass('is-fixed');
 
