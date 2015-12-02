908b7ce0fb8457173df70812e962c718980027e8
pashaOliinyk <pashades@gmail.com>
2015-12-02 18:29:30 +0200

    Ref: structure of scss, optimize sidebar-menu

--- scripts/components/pd-side-menu.js
+++ scripts/components/pd-side-menu.js
@@ -1,12 +1,16 @@
 (function(){
-    $(document).ready(function() {
-        var secondaryNav = $('#sideMenu');
-        var offset = 307;
+    $(window).load(function() {
+        var secondaryNav = $('.aside-menu'),
+            offset = secondaryNav.offset().top,
+            firstLiElem = $(".aside-menu ul").first().css('margin-top').replace("px", '');
+
+        Number(firstLiElem);
+        Math.round(offset);
+        offset = offset - firstLiElem;
 
         $(window).on('scroll', function () {
             if ($(window).scrollTop() > offset) {
                 secondaryNav.addClass('is-fixed');
-
             } else {
                 secondaryNav.removeClass('is-fixed');
             }
