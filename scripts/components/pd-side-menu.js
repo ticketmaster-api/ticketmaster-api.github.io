4fea08655e5d8cbf3e539726d955950870ae67c1
pashaOliinyk <pashades@gmail.com>
2015-12-07 14:49:49 +0200

    Add: html parser, styles Ref: side-menu fixed styles&js

--- scripts/components/pd-side-menu.js
+++ scripts/components/pd-side-menu.js
@@ -1,24 +1,16 @@
 (function(){
     $(document).ready(function() {
-        var secondaryNav = $('.aside-menu'),
-            secondaryNavMargin = secondaryNav.first().css('margin-top').replace("px", ''),
-            firstLiElem = secondaryNav.first().css('height').replace("px", ''),
-            wrapperMenu = $(".wrapper-aside-menu").offset().top,
-            offset = $('.aside-menu').offset().top;
-
-        firstLiElem = parseFloat(firstLiElem);
-        offset = offset-secondaryNavMargin;
+        var secondaryNav = $('.aside-menu');
+        var offset = $('.aside-menu').offset().top;
 
         $(window).on({
             resize:function(){
-                firstLiElem = $('.menu-header').first().css('height').replace("px", '');
-                firstLiElem = parseFloat(firstLiElem);
-                offset = wrapperMenu + firstLiElem;
+                offset = $('.aside-menu').offset().top;
             },
             scroll:function(){
                 var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page (will be used for active-nav class)
 
-                if (windowPos > offset) {
+                if ($(window).scrollTop() > offset) {
                     secondaryNav.addClass('is-fixed');
                 } else {
                     secondaryNav.removeClass('is-fixed');
