ee113898f1a2625fb4e7e9d6083d3df20c079cd2
Vitalii_Menshutin <Vitalii_Menshutin@epam.com>
2015-12-08 13:31:58 +0200

    fix: minor UI fixes for Home and Components

--- scripts/components/parser.js
+++ scripts/components/parser.js
@@ -14,23 +14,21 @@
         }
         /*Normalize END*/
 
-        main.find('.article').each(
-            function () {
-                var group = $(this).nextUntil('.article').addBack();
-                group.wrapAll('<div class="article-wrapper"></div>');
-            });
+        main.find('.article').each(function () {
+            var group = $(this).nextUntil('.article').addBack();
+            group.wrapAll('<div class="article-wrapper"></div>');
+        });
 
-        main.find('.aside').each(
-            function () {
-                var me = $(this);
-                var group = me.nextUntil('.article').addBack();
-                var groupLeft = me.parent().children().first().nextUntil('.aside').addBack();
+        main.find('.aside').each(function () {
+            var me = $(this);
+            var group = me.nextUntil('.article').addBack();
+            var groupLeft = me.parent().children().first().nextUntil('.aside').addBack();
 
-                group.wrapAll('<div class="aside-wrapper"></div>');
+            group.wrapAll('<div class="aside-wrapper"></div>');
 
-                groupLeft.wrapAll('<div class="left-wrapper"></div>');
-                //console.log($('*').length);//As less is better
-            });
+            groupLeft.wrapAll('<div class="left-wrapper"></div>');
+            //console.log($('*').length);//As less is better
+        });
 
 
     })
