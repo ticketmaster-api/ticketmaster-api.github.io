744c4af56210868cb0034ab95008694e9bdfed4c
Vitalii_Menshutin <Vitalii_Menshutin@epam.com>
2015-12-10 16:36:20 +0200

    mod: TKM-DPA-12 working

--- scripts/components/pd-side-menu.js
+++ scripts/components/pd-side-menu.js
@@ -5,13 +5,38 @@
             sideBtn = $("#side-menu-btn"),
             asideBlock = $("#aside-block"),
             topBar = $('.top-bar').addClass('menu-bg'),
-            bottomBar = $('#footer').addClass('menu-bg');
+            bottomBar = $('#footer').addClass('menu-bg'),
+            menu = $('ul.aside-menu'),
+            $body = $('body').addClass('menu-bg');
 
-        $('body').addClass('menu-bg');
+        var showMenu = function(){
+            asideBlock.addClass("has-bg");
+            topBar.addClass('menu-bg');
+            bottomBar.addClass('menu-bg');
+            $body.addClass('menu-bg');
+            menu.show();
+            sideBtn.addClass("expanded").removeClass("closed");
+        };
+
+        var hideMenu = function(){
+            asideBlock.removeClass("has-bg");
+            topBar.removeClass('menu-bg');
+            bottomBar.removeClass('menu-bg');
+            $body.removeClass('menu-bg');
+            menu.hide();
+            sideBtn.addClass("closed").removeClass("expanded");
+        };
+
+        if (Math.max(document.documentElement.clientWidth, window.innerWidth || 0) < 1200){
+            hideMenu();
+        }
 
         $(window).on({
-            resize:function(){
-                offset = menuWraper.offset().top;
+            resize: function(){
+                var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+                if (w >= 1200){
+                    showMenu();
+                }
             },
             scroll:function(){
                 if ($(window).scrollTop() > offset) {
@@ -23,9 +48,12 @@
         });
 
         sideBtn.on("click", function(){
-            asideBlock.toggleClass("bg-grey");
-            topBar.toggleClass('menu-bg');
-            bottomBar.toggleClass('menu-bg');
+            if ($(this).hasClass("closed")){
+                showMenu();
+            }
+            else {
+                hideMenu();
+            }
         });
     })
 
