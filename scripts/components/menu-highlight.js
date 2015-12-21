6622787bdb872bc72686983bb7a5ac54cf6a306c
RaNKaR <esc.puf@gmail.com>
2015-12-21 17:02:37 +0200

    [fix,add]: added new highlight script

---
+++ scripts/components/menu-highlight.js
@@ -0,0 +1,32 @@
+/**
+ *  Menu highlight
+ **/
+(function(){
+    var topMenu = $(".menu-highlight"),
+        topMenuHeight = topMenu.outerHeight()+15,
+        // All list items
+        menuItems = topMenu.find("a"),
+        // Anchors corresponding to menu items
+        scrollItems = menuItems.map(function(){
+            var item = $($(this).attr("href"));
+            if (item.length) { return item; }
+        });
+
+    $(window).scroll(function(){
+        // Get container scroll position
+        var fromTop = $(this).scrollTop()+topMenuHeight;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function(){
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length-1];
+        var id = cur && cur.length ? cur[0].id : "";
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href=#"+id+"]").parent().addClass("active");
+    });
+})();
