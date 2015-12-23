77c6d0b1fc892fb197f6f18df5173a67b31c6182
pashaOliinyk <pashades@gmail.com>
2015-12-23 10:23:41 +0200

    Ref: set tabs show next N-elements using siblings

--- scripts/components/parser.js
+++ scripts/components/parser.js
@@ -40,46 +40,38 @@
                     //move first element to class="aside-wrapper"
                     firstElemGroupLeft.prependTo( firstElemGroupLeft.parent().parent() );
 
-                    //add class for tabs
+                    /*parse tabs*/
                     me.children().children().first().addClass('active')//set first button active
 
-                    $(".aside-wrapper blockquote").nextAll(".highlight").addClass('tab-content' ); //hide all tab-content
-                    me.children(".highlight:lt("+ tabsCount +")");
+                    tabsCount = $('.aside-wrapper blockquote a').first().nextUntil('p').length+1;
+
+                    me.siblings(':lt('+tabsCount+')')
+                        .addClass('tab-content')
+                        .addClass(function (index) {
+                            if (index > tabsCount) {
+                                index = [index % (tabsCount + 1)]
+                            }
+                            return "tab-" + index;
+                        })
+                        .attr("id", function (index) {
+                            if (index > tabsCount) {
+                                index = [index % (tabsCount + 1)]
+                            }
+                            return "tab-" + index;
+                        });
 
                     group.nextAll().first().addClass('tab-active');//set first tab visible
+                    /*parse tabs end*/
                 }
-                //console.log($('*').length);//As less then better
+                //console.log($('*').length);//As less is better
             });
 
-        //add class and id to tab content
-        tabsCount = $('.aside-wrapper blockquote a').first().nextUntil('p').length;
+        //add class to move it top
         $(".aside-wrapper > blockquote").parent('.aside-wrapper').addClass('tab-panel-offset');
-
-        //add classes to tab-content
-        tabsCount = tabsCount+1;
-        main.find('.tab-panel-offset').each(
-            function () {
-                var me = $(this);
-                //console.log('sibling' , me.children().siblings(":nth-child(n+1)").nextUntil(":nth-child(6)") );
-                me.children(".highlight:lt("+ tabsCount +")")
-                    .addClass(function (index) {
-                        if (index > tabsCount) {
-                            index = [index % (tabsCount + 1)]
-                        }
-                        return "tab-" + index;
-                    })
-                    .attr("id", function (index) {
-                        if (index > tabsCount) {
-                            index = [index % (tabsCount + 1)]
-                        }
-                        return "tab-" + index;
-                    });
-            });
 
         if(firstElem.hasClass('underline')){
             firstElem.css('margin-right','51%');
         }
-
 
         $('table').wrap('<div class="table-wrapper"></div>');
 
@@ -93,13 +85,12 @@
 
         $(".lang-selector a").click(function(event) {
             var currentButton =$(this);
-            var allBtn = $(".lang-selector a[href*=" + currentButton.attr('href') + "]")
+            var allBtn = $(".lang-selector a[href*=" + currentButton.attr('href') + "]");
+            var tabGroup = $('.tab-active');
 
             event.preventDefault();
             allBtn.addClass("active");
             allBtn.siblings().removeClass("active");
-
-            var tabGroup = $('.tab-active');
 
             if(allBtn.hasClass('active')){
                 var strTMP = currentButton.index().toString();
