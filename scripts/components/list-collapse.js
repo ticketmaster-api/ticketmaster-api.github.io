4a0ecc33db55ae8a27be8dcef2533c1a67e3ee57
pashaOliinyk <pashades@gmail.com>
2016-02-17 19:05:50 +0200

    Add: jsTree collapse

--- scripts/components/list-collapse.js
+++ scripts/components/list-collapse.js
@@ -1,19 +1,71 @@
-function onExpandNodeClick() {
-    var expandLink = this;
+$('.nested-list').wrap("<div class='nested-list-wrapper'></div>");
 
-    var container = $(this).siblings(".expand-container");
-    container.slideToggle({
-        duration: "fast",
-        complete: function () {
-            if ($(this).is(":visible")) {
-                $(expandLink).removeClass("collapsed").addClass("expanded");
-            } else {
-                $(expandLink).removeClass("expanded").addClass("collapsed");
-            }
+
+$('.nested-list-wrapper').jstree({
+    "core" : { // core options go here
+        "multiple" : false, // no multiselection
+        "themes" : {
+            "dots" : true // no connecting dots between dots
+        },
+        "dblclick_toggle":false
+    },
+    "plugins" : ["state"] // activate the state plugin on this instance
+});
+
+$('.nested-list-wrapper')
+    .on('before_open.jstree', function () {
+        console.log('before_open');
+        $('.jstree-themeicon').remove(); //remove needless element
+    })
+    .on('select_node.jstree', function (e, data) {
+        data.instance.toggle_node(data.node); //set open on one click
+    })
+    /*.on('hover_node.jstree', function (e, data) {
+     console.log( $('#'+data.node.id) );
+     $('#'+data.node.id + '> i').addClass(hoveredCssClass);
+
+     })*/
+    .jstree();
+
+var hoveredCssClass  = 'hovered';
+
+function toggleTargetElement() {
+    var me = $( this );
+    if (me.is('a')){
+        console.log(' return  i');
+        return 'i'
+    }else {
+        console.log(' return  a');
+        return 'a';
+    }
+};
+
+function toggleIconClass(toggle) {
+    var iconElement;
+    var me = $( this );
+
+    /*if (me.is('a')){
+     iconElement = me.prev('i');
+     }else {
+     iconElement = me.next('a');
+     }*/
+
+    iconElement = me.prev('i');
+    if(toggle){
+        iconElement.addClass(hoveredCssClass);
+    }else{
+        iconElement.removeClass(hoveredCssClass);
+    }
+};
+
+
+$('.jstree-container-ul')
+    .on({
+        mouseenter: function(){
+            console.log('mouseenter');
+            toggleIconClass.call(this, true );
+        },
+        mouseleave: function(){
+            toggleIconClass.call(this, false );
         }
-    });
-
-    return false;
-}
-
-$("a.expand-link.contains-children-true").click(onExpandNodeClick);
+    },'a');
