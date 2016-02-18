ea21a34679b051b893b65ae47811c962e6487e5e
Oleksandr Kovalov <oleksandr_kovalov@epam.com>
2016-02-18 12:48:59 +0200

    [mod] Top Search Fix

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -82,23 +82,29 @@
 
                 // Search [START]
 
+                var smopen  = false;
                 $(".top-bar").on("click", "#search", function (e) {
                   if ( $(this).hasClass("smopen") ) {
                       $(this).removeClass("smopen");
                       $("#cse-search-box").removeClass("sopen");
+                      smopen  = false;
                   }
                   else {
                       $(this).addClass("smopen");
                       $("#cse-search-box").addClass("sopen");
                       $("input.q").focus();
+                      smopen  = true;
                   }
                 });
 
                 $("input.q").blur(function(e) {
-                    setTimeout(function () {
-                        $("#cse-search-box").removeClass("sopen");
-                        $("#search").removeClass("smopen");
-                    }, 127);
+                    if (smopen == true) {
+                        setTimeout(function () {
+                            $("#cse-search-box").removeClass("sopen");
+                            $("#search").removeClass("smopen");
+                            smopen = false;
+                        }, 127);
+                    }
                 });
 
                 $("#search").on("click", ".search-button", function (e) {
