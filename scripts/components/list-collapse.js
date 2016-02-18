39acb7a67f8d6bd18fd5d23ecf8bd4cfa4afab57
pashaOliinyk <pashades@gmail.com>
2016-02-18 14:53:28 +0200

    Mod: style of list

--- scripts/components/list-collapse.js
+++ scripts/components/list-collapse.js
@@ -1,71 +1,69 @@
-$('.nested-list').wrap("<div class='nested-list-wrapper'></div>");
+(function () {
+    $(document).ready(function () {
+
+        $('.nested-list').wrap("<div class='nested-list-wrapper'></div>");
+        $('.jstree-themeicon').remove(); //remove needless icon element
+
+        $('.nested-list-wrapper')
+            .on('before_open.jstree', function () {
+                $('.jstree-themeicon').remove(); //remove needless element
+            })
+            .on('select_node.jstree', function (e, data) {
+                data.instance.toggle_node(data.node); //set open on one click
+            })
+            .jstree({
+                "core": {
+                    "themes": {
+                        "dots": true // no connecting dots between dots
+                    },
+                    "dblclick_toggle": false
+                },
+                "plugins": ["state"] // activate the state plugin on this instance
+            });
 
 
-$('.nested-list-wrapper').jstree({
-    "core" : { // core options go here
-        "multiple" : false, // no multiselection
-        "themes" : {
-            "dots" : true // no connecting dots between dots
-        },
-        "dblclick_toggle":false
-    },
-    "plugins" : ["state"] // activate the state plugin on this instance
-});
+        var hoveredCssClass = 'hovered';
 
-$('.nested-list-wrapper')
-    .on('before_open.jstree', function () {
-        console.log('before_open');
-        $('.jstree-themeicon').remove(); //remove needless element
-    })
-    .on('select_node.jstree', function (e, data) {
-        data.instance.toggle_node(data.node); //set open on one click
-    })
-    /*.on('hover_node.jstree', function (e, data) {
-     console.log( $('#'+data.node.id) );
-     $('#'+data.node.id + '> i').addClass(hoveredCssClass);
+        function toggleIconClass(toggle) {
+            var iconElement,
+                me = $(this);
 
-     })*/
-    .jstree();
+            if (me.is('a')) {
+                iconElement = me.prev('i');
+            } else {
+                iconElement = me.next('a');
+            }
 
-var hoveredCssClass  = 'hovered';
+            if (toggle) {
+                iconElement.addClass(hoveredCssClass);
+            } else {
+                iconElement.removeClass(hoveredCssClass);
+            }
+        };
 
-function toggleTargetElement() {
-    var me = $( this );
-    if (me.is('a')){
-        console.log(' return  i');
-        return 'i'
-    }else {
-        console.log(' return  a');
-        return 'a';
-    }
-};
-
-function toggleIconClass(toggle) {
-    var iconElement;
-    var me = $( this );
-
-    /*if (me.is('a')){
-     iconElement = me.prev('i');
-     }else {
-     iconElement = me.next('a');
-     }*/
-
-    iconElement = me.prev('i');
-    if(toggle){
-        iconElement.addClass(hoveredCssClass);
-    }else{
-        iconElement.removeClass(hoveredCssClass);
-    }
-};
+        /*hover on a*/
+        $('.jstree-container-ul')
+            .on({
+                mouseenter: function () {
+                    toggleIconClass.call(this, true);
+                },
+                mouseleave: function () {
+                    toggleIconClass.call(this, false);
+                }
+            }, 'i');
 
 
-$('.jstree-container-ul')
-    .on({
-        mouseenter: function(){
-            console.log('mouseenter');
-            toggleIconClass.call(this, true );
-        },
-        mouseleave: function(){
-            toggleIconClass.call(this, false );
-        }
-    },'a');
+        /*hover on icon*/
+        $('.jstree-container-ul')
+            .on({
+                mouseenter: function () {
+                    toggleIconClass.call(this, true);
+                },
+                mouseleave: function () {
+                    toggleIconClass.call(this, false);
+                }
+            }, 'a');
+
+
+    });
+})();
