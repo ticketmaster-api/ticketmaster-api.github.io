f0cee1eb3e26730ee3ef51a8932053ef9e4e1406
pashaOliinyk <pashades@gmail.com>
2016-02-25 14:01:56 +0200

    add: ok-alert

--- scripts/components/feedback.js
+++ scripts/components/feedback.js
@@ -1,9 +1,12 @@
 (function($){
 
     var $modal = $('#feedback-modal'),
+        $modalAlert = $('#feedback-alert-modal'),
         $form = $modal.find('#js_feedback_form'),
         $email = $form.find('#email'),
         $btn = $modal.find('#js_feedback_btn'),
+        $btnAlert = $modal.find('#js_feedback_btn_alert'), //tmp
+        $btnAlertOk = $modal.find('#js_feedback_btn_alert'),
         cssValidationClass = 'feedback_form-validation';
 
     function resetForm(){
@@ -27,12 +30,19 @@
             url: $form.attr('action'),
             data: $form.serialize()
         }).done(function() {
-            //alert("Thank you.");
+            $modalAlert.modal();
+            //showErrorPopup('Thank you for contacting us!','We will review and respond promptly.');
         });
 
         // Close dialog
         $modal.modal('hide');
     }
+
+    var showErrorPopup = function(message,message2){
+        var alert = $('#alert-modal');
+        alert.find('#modal-body').text(message).text(message2);
+        alert.modal();
+    };
 
     // EVENTS
     $btn.on('click', function(){
@@ -46,6 +56,15 @@
             $form.addClass(cssValidationClass);
         }
     });
+    $btnAlertOk.on('click', function(){
+        $modalAlert.modal('hide');
+    });
+
+    //show test modal
+    $btnAlert.on('click', function(){
+        $modalAlert.modal(); //gray button
+    });
+
 
     $modal.on('hide.bs.modal', resetForm);
 
