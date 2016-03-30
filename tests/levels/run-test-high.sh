5652544c3dd399e61a0634136f1ac3be1490a65f
pashaOliinyk <pashades@gmail.com>
2016-03-30 17:16:00 +0300

    add: testing levels

---
+++ tests/levels/run-test-high.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "HIGH test starts"
+
+galen test tests/galen/all.test --htmlreport tests/galen/reports/all --groups HomePage
+galen test tests/galen/all-part2.test --htmlreport tests/galen/reports/all-part2 --groups PartnersLanding #in progress
+galen test tests/galen/all-part2.test --htmlreport tests/galen/reports/all-part2 --groups SupportPage
+galen test tests/galen/all-part2.test --htmlreport tests/galen/reports/all-part2 --groups BlogsPage
+galen test tests/galen/all-part2.test --htmlreport tests/galen/reports/all-part2 --groups Events
