69041a850c304c05e1a1904e145a0ce0846d2920
Roman_Prytula <pritularoman1985@gmail.com>
2016-04-01 17:04:39 +0300

    [fix] Mobile widget configurator. Issue with 'Go' button.

--- widgets/main/lib/main-widget-config.js
+++ widgets/main/lib/main-widget-config.js
@@ -140,6 +140,11 @@
       $widgetModalNoCode = $('#js_widget_modal_no_code');
 
   $configForm.on("change", changeState);
+  // Mobile devices. Force 'change' by 'Go' press
+  $configForm.on("submit", function (e) {
+    $configForm.find('input:focus').trigger('blur');
+    e.preventDefault();
+  });
 
   $configForm.find("input[type='text']").each(function () {
     var $self = $(this);
