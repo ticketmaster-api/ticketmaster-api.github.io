093d3cb37ea4bfab7824ef3d24a74cef6b20c61e
Ievgen Tsekhmeistruk <Ievgen_Tsekhmeistruk@epam.com>
2016-08-30 20:00:44 +0300

    - change browsser to Firefox - add static method: scrollToElement - use scrollToElement method to resolve Firefox issues in necessary places - minor changes

--- tests/serenity/src/main/java/bla/tm/steps/products_and_docs/PD_SDKsSteps.java
+++ tests/serenity/src/main/java/bla/tm/steps/products_and_docs/PD_SDKsSteps.java
@@ -1,12 +1,14 @@
 package bla.tm.steps.products_and_docs;
 
 import bla.tm.pages.site.products_and_docs.PD_SDKsPage;
+import gherkin.lexer.Th;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 
 import static bla.tm.staticmethods.StaticMethods.findWebElementByKey;
+import static bla.tm.staticmethods.StaticMethods.scrollToElement;
 import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
 import static org.junit.Assert.assertEquals;
 
@@ -39,12 +41,9 @@ public class PD_SDKsSteps {
         WebElementFacade image = findWebElementByKey(key, sDKsPage.getClickableImages());
         WebElementFacade element = findWebElementByKey(key, sDKsPage.getClickableElements());
         Actions actions = new Actions(getDriver());
-        actions.moveToElement(image).moveToElement(element).click(element).perform();
-
-//        image.click();
-//        image.click();
-//        image.click();
-//        element.click();
+        actions.moveToElement(image).moveToElement(element).build().perform();
+        scrollToElement(element);
+        actions.click(element).build().perform();
     }
 
 }
