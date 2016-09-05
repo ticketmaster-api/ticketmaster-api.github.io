193998343f65f28a18022a8efa0d2691695441fa
Ievgen Tsekhmeistruk <Ievgen_Tsekhmeistruk@epam.com>
2016-09-05 15:51:45 +0300

    - add negative scenario related to ApiKey replacement on Inventory Status API page

--- tests/serenity/src/test/java/bla/tm/definitions/site/products_and_docs/PD_InventoryStatusAPIDefinition.java
+++ tests/serenity/src/test/java/bla/tm/definitions/site/products_and_docs/PD_InventoryStatusAPIDefinition.java
@@ -1,18 +1,38 @@
 package bla.tm.definitions.site.products_and_docs;
 
+import bla.tm.steps.pantheon.UserAccountSteps;
+import bla.tm.steps.pantheon.UserLogInSteps;
 import bla.tm.steps.products_and_docs.PD_InventoryStatusAPISteps;
 import net.thucydides.core.annotations.Steps;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
 
 public class PD_InventoryStatusAPIDefinition {
+
+    private String apiKey = "{apikey}";
 
     @Steps
     PD_InventoryStatusAPISteps inventoryStatusAPIPage;
 
+    @Steps
+    UserLogInSteps userLogInPage;
+
+    @Steps
+    UserAccountSteps userAccountSteps;
+
     @Given("open Inventory Status API page")
     public void openInventoryStatusAPIPage() {
+        inventoryStatusAPIPage.closePage();
         inventoryStatusAPIPage.maximiseBrowserWindow();
+        inventoryStatusAPIPage.openPage();
+    }
+
+    @When("User is logged to site (Inventory Status API)")
+    public void openLogInPageAndLogIn() {
+        inventoryStatusAPIPage.clickLogIn();
+        userLogInPage.logInToAccount();
+        apiKey = userAccountSteps.getAPIKeyOfUser();
         inventoryStatusAPIPage.openPage();
     }
 
@@ -22,4 +42,8 @@ public class PD_InventoryStatusAPIDefinition {
         inventoryStatusAPIPage.checkGeneralPageElements(disqus, leftMenu);
     }
 
+    @Then("check that API key is provided for all placeholder on Inventory Status API page")
+    public void checkAPIKeyPlaceholders(){
+        inventoryStatusAPIPage.checkAPIKeyPlaceholders(apiKey);
+    }
 }
