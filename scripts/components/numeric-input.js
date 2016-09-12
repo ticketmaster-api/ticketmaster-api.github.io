da27dda23410fd4db371a4548268a134a9731ec1
pashaOliinyk <pashades@gmail.com>
2016-09-12 18:04:34 +0300

    add: custom input component

--- scripts/components/numeric-input.js
+++ scripts/components/numeric-input.js
@@ -23,11 +23,6 @@
             if (!( $.isNumeric(value) )) {
                 value = 0;
             }
-            /*if (input.attr('step')) {
-                step = parseFloat(input.attr('step'));
-            } else {
-                step = 1;
-            }*/
             step = getStep(input);
         }
 
@@ -35,40 +30,40 @@
             return (input.attr('step')) ? parseFloat(input.attr('step')): 1;
         }
 
+        function getParsedValue(inputElement , min) {
+            var val = ( settings.floatVal || inputElement.data('float') === true )? parseFloat( inputElement.val() ) : parseInt( inputElement.val() );
+            if(min && val !== val) {
+                // console.info('val is NaN.');
+                val = min;
+            }
+            else {/* console.info('val is NOT a NaN.');*/ }
+            return val;
+        }
+
+
         /**
          * Check value when it entered from keybord
          * @param inputElement - object
          * @param min - number
          * @param max - number
          */
-        function checkValue(inputElement, min , max ) {
-            var val = ( settings.floatVal )? parseFloat( inputElement.val() ) : parseInt( inputElement.val() ) ,//remove letters
-                vlidatedValue = 0;
-            console.group();
-                console.log('val',val , 'vlidatedValue 1 ', vlidatedValue , 'defaults.floatVal' , defaults.floatVal);
-                console.log('min ',min,'max ',max);
+        function checkValue(inputElement, min , max ,incBtn,  decBtn) {
+            var val = getParsedValue(inputElement, min) ;
 
-            if(val < min){
+            if(val <= min){
                 val = min;
-                vlidatedValue = min;
-                console.log('val <= min -> ', inputElement.val() );
+                decBtn.addClass('disabled');
+                incBtn.removeClass('disabled');
 
-            } else if (val > max){
+            } else if (val >= max){
                 val = max;
-                vlidatedValue = max;
-                console.log('if(val >= max) -> ', inputElement.val() );
-
+                incBtn.addClass('disabled');
+                decBtn.removeClass('disabled');
             }
             if(!( $.isNumeric(val) )){
                 val = '';
             }
             inputElement.val(val);
-            inputElement.siblings('[type=number]').val(val);
-
-                console.log('if(min<= val && val <=max) -> ', inputElement.val() );
-                console.log('settings ', settings );
-                console.log('vlidatedValue ', vlidatedValue );
-            console.groupEnd();
         }
 
         return this.each(function() {
@@ -80,80 +75,68 @@
 
             init($numeric_input);
 
-            incBtn =  $(this).siblings( ".arrow__inc" );
-            decBtn =  $(this).siblings( ".arrow__dec" );
+            incBtn =  $(this).siblings( ".arrow__inc" ); //find increment button
+            decBtn =  $(this).siblings( ".arrow__dec" ); //find decrement button
 
-            // console.log('$incButton', incBtn);
-            // console.log('$(this).next( ".arrow__dec" )', $(this));
-            // console.log('decBtn', decBtn);
+
 
             /*
             $numeric_input.focus(function() {
-                $(this).prop('type', 'text')
-                  .keyup(function() {
-                      $(this).val($(this).val().replace(/[^\d|\s]/g, ''));
-                      //checkValue($(this), $min, $max);
-                  })
+                $(this)
+                  //.prop('type', 'text')
+                  // .keyup(function() {
+                  //     $(this).val($(this).val().replace(/[^\d|\s]/g, ''));
+                  //     //checkValue($(this), $min, $max);
+                  // })
                   .blur(function() {
-                      $(this).val($(this).val().replace(/[^\d]/g, '')).prop('type', 'number');
+                      //$(this).val($(this).val().replace(/[^\d]/g, '')).prop('type', 'number');
                   });
             });*/
 
             $numeric_input.change(function(){
-                checkValue($(this), $min, $max);
+                checkValue($(this), $min, $max , incBtn , decBtn); //need for update "widget config panel"
             });
 
             incBtn.click(function() {
-
-                var value = parseInt($numeric_input.val());
+                var value = getParsedValue($numeric_input, null);
                 var step = getStep($numeric_input);
-                // var val = value + step;
-                var idd = $numeric_input.attr('id');
                 var newValue;
 
                 checkInputAttr($numeric_input);
                 newValue = value + step;
-                console.log('max' , $max , newValue);
+
                 if (newValue >= $max) {
                     newValue = $max;
                     $(this).addClass('disabled');
                 }else {
-                    console.log('show decBtn');
                     decBtn.removeClass('disabled');
                 }
 
                 $numeric_input.val(newValue);
-                $numeric_input.attr( "value", newValue );
-
-                console.log('newValue' , newValue);
-                $numeric_input.trigger('change');
+                $numeric_input.attr( "value", newValue );//update  value attribute, need for "widget config panel"
+                $numeric_input.trigger('change');//need for update "widget config panel"
 
                 return false;
             });
 
             decBtn.click(function() {
                 var newValue;
-                var value = parseInt($numeric_input.val());
+                var value = getParsedValue($numeric_input, null);
                 var step = getStep($numeric_input);
-                var idd = $numeric_input.attr('id');
-
-                console.log('min' , $min);
 
                 checkInputAttr($numeric_input);
                 newValue = value - step;
+
                 if (newValue <= $min) {
                     newValue = $min;
                     $(this).addClass('disabled');
                 }else {
-                    console.log('show incBtn');
                     incBtn.removeClass('disabled');
                 }
 
                 $numeric_input.val(newValue);
-                $numeric_input.attr( "value", newValue );
-                console.log('newValue' , newValue);
-
-                $numeric_input.trigger('change');
+                $numeric_input.attr( "value", newValue ); //update  value attribute, need for "widget config panel"
+                $numeric_input.trigger('change'); //need for update "widget config panel"
                 
                 return false;
             });
@@ -163,5 +146,13 @@
 })(jQuery);
 
 $(document).on('ready', function () {
+    /*
+    availiable option : {floatVal:true}
     $('.js_numeric_input').checkNumeric( {floatVal:true} );
+    or add attribute to html: data-float="true"
+
+    markup example:
+     <input class="js_numeric_input" type="number" value="12" max="15" min="10" required="" step="2" data-float="true">
+    */
+    $('.js_numeric_input').checkNumeric( );
 });
