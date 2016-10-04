94cd8ec3c671b03a151e0e835db2c48fdb2929bd
Ievgen Tsekhmeistruk <Ievgen_Tsekhmeistruk@epam.com>
2016-10-04 09:36:20 +0300

    - automate general checking for UNIVERSAL CHEKOUT Page and WORDPRESS Widget Page

---
+++ tests/serenity/src/main/java/bla/tm/pages/site/products_and_docs/PD_Widget_UniversalCheckoutPage.java
@@ -0,0 +1,9 @@
+package bla.tm.pages.site.products_and_docs;
+
+import bla.tm.pages.AncestorPage;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("/products-and-docs/widgets/checkout/")
+public class PD_Widget_UniversalCheckoutPage extends AncestorPage{
+    public final String pageHeader = "UNIVERSAL CHECKOUT";
+}
