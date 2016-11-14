9636aa39b740da0109209bc668d40846f97a24c3
Pavlo Komenda <Pavlo_Komenda@epam.com>
2016-11-14 15:04:53 +0200

    [TKMDPA-1325][JS?SCSS/HTML] Tablet responsive layout

--- scripts/api-explorer/v2/src/modules/slider.js
+++ scripts/api-explorer/v2/src/modules/slider.js
@@ -1,37 +1,60 @@
+const config = {
+	dots: false,
+	infinite: false,
+	speed: 300,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	autoplay: false,
+	focusOnSelect: true,
+	responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				dots: true,
+				slidesToShow: 2,
+				slidesToScroll: 1,
+			}
+		},
+		{
+			breakpoint: 800,
+			settings: {
+				dots: true,
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+	]
+};
+
+const config2 = {
+	variableWidth: true,
+	autoplay: false,
+	responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				variableWidth: true,
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 800,
+			settings: {
+				variableWidth: true,
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+	]
+};
+
 function slick(times) {
 	"use strict";
 	var selector = '#slider-';
-	
+
 	for (var i = 0; i < times; i++) {
-		$(selector + i).length && $(selector + i).slick({
-			dots: false,
-			infinite: false,
-			speed: 300,
-			slidesToShow: 3,
-			slidesToScroll: 1,
-			variableWidth: true,
-			autoplay: false,
-			responsive: [
-				{
-					breakpoint: 1200,
-					settings: {
-						variableWidth: true,
-						slidesToShow: 2,
-						slidesToScroll: 1,
-						infinite: false,
-						dots: false
-					}
-				},
-				{
-					breakpoint: 800,
-					settings: {
-						variableWidth: true,
-						slidesToShow: 1,
-						slidesToScroll: 1
-					}
-				}
-			]
-		});
+		$(selector + i).length && $(selector + i).slick(config);
 	}
 }
 
