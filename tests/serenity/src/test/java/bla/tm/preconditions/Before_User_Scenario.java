d87cfa1bc8adcef1505b67f55cfb597bc7e904f4
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-01-23 10:43:16 +0200

    Revert "Changed clearCookies method to beforeScenario"
    This reverts commit 3bdbc726522b72b5916c248bbf8159135ee2a21c.

--- tests/serenity/src/test/java/bla/tm/preconditions/Before_User_Scenario.java
+++ tests/serenity/src/test/java/bla/tm/preconditions/Before_User_Scenario.java
@@ -1,15 +1,10 @@
-package bla.tm.preconditions;
-
-import bla.tm.steps.AnyPageSteps;
-import org.jbehave.core.annotations.BeforeScenario;
-
-public class Before_User_Scenario {
-
-    AnyPageSteps anyPageSteps;
-
-    @BeforeScenario()
-    public void clearCookiesAndLocalStorage (){
-        anyPageSteps.clearCookiesAndLocalStorage();
-    }
-
-}
+//package bla.tm.preconditions;
+//
+//import org.jbehave.core.annotations.BeforeScenario;
+//
+//public class Before_User_Scenario {
+//    @BeforeScenario()
+//    public void beforeEachScenario (){
+//        //        System.out.println("Before Scenario ...");
+//    }
+//}
