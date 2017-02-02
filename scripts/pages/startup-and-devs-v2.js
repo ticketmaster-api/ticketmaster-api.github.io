bab1be0b1cfb8d38b8bf61dc4a657fcb0985b684
Pavlo Komenda <Pavlo_Komenda@epam.com>
2017-02-02 14:58:55 +0200

    [TKMDPA-4143][js/scss] fixes tablet view

--- scripts/pages/startup-and-devs-v2.js
+++ scripts/pages/startup-and-devs-v2.js
@@ -1,102 +1,113 @@
 'use strict';
 
 (function($) {
-	$('#myCarousel').slick({
-		dots: true,
-		lazyLoad: 'ondemand',
-		infinite: true,
-		speed: 300,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 10000,
-		fade: true,
-		cssEase: 'linear',
-	});
-
-	$('.card').each(function() {
-		let me = $(this);
-		me.hover(function() {
-			me.toggleClass('applyflip');
+	var initHeaderSlider = function ($sliderRootElement) {
+		$sliderRootElement.slick({
+			dots: true,
+			lazyLoad: 'ondemand',
+			infinite: true,
+			speed: 300,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 10000,
+			fade: true,
+			cssEase: 'linear',
 		});
-	});
+	},
+		initCardFlipper = function ($cards) {
+			$cards.each(function() {
+				var me = $(this);
+				me.hover(function() {
+					me.toggleClass('applyflip');
+				});
+			});
+		},
+		initGalleryGrid = function ($grid) {
+			$grid.masonry({
+				itemSelector: '.grid-item',
+				transitionDuration: '0.2s',
+			});
+		},
+		setEventListeners = function ($rootTarget, $grid) {
+			$rootTarget
+				.on('click touch', '#tab-dropdown li, #tab-controlls li', function(e) {
+					var text = $(this)
+						.find('.tab')
+						.text(),
+						$tab = $('.tab:contains("' + text + '")');
 
-	let $grid = $('.grid').masonry({
-		itemSelector: '.grid-item',
-		transitionDuration: '0.2s',
-	});
+					$('#tab-dropdown li, #tab-controlls li')
+						.removeClass('active')
+						.find('.tab')
+						.removeAttr('aria-expanded');
 
-	let cards;
+					$tab
+						.attr('aria-expanded', true)
+						.parent()
+						.addClass('active');
 
-	$(document)
-		.on('ready', function(e) {
-			let text = $('#tab-controlls > li.active').text(),
-				$window = $(window),
-				size = 780;
-			cards = $('.card');
+					$('#tab-dropdown-label').text(text);
 
-			$('#tab-dropdown-label').text(text);
-
-			if ($window.width() <= size) {
-				cards.toggleClass('card static-card');
-				let $tab = $('.tab:contains("' + text + '")');
-
-				$tab
-					.attr('aria-expanded', true)
-					.parent()
-					.addClass('active');
-			}
-
+					$('#tab-dropdown').removeClass('open');
+				})
+				.on('click touch', '#gallery-show-more, #top-show-more', function(e) {
+					e.stopPropagation();
+					var selector = $(this).attr('aria-controls');
+					$(selector).addClass('more');
+					$(this).addClass('hide');
+				})
+				.on('click touch', '.btn-like:enabled', function(e) {
+					e.stopPropagation();
+					e.preventDefault();
+					var count = $(this).parent('.like').find('.count');
+					count.text(+count.text() + 1);
+					$(this).attr('disabled', true);
+				})
+				.on('click touch', '.tab', function(e) {
+					setTimeout(function() {
+						$grid.masonry('layout');
+					}, 0);
+				})
+				.on('click touch', '#tab-dropdown .tab', function(e) {
+					var text = $(this).text();
+					$('#tab-dropdown-label').text(text);
+				});
+		},
+		windowResizeAction = function ($window, size, $cards) {
 			$window.on('resize', function() {
-				if (($window.width() <= size && cards.hasClass('card')) ||
-					($window.width() > size && !cards.hasClass('card'))) {
-					cards.toggleClass('card static-card');
+				if (($window.width() <= size && $cards.hasClass('card')) ||
+					($window.width() > size && !$cards.hasClass('card'))) {
+					$cards.toggleClass('card static-card');
 				}
 			});
-		})
-		.on('click touch', '#tab-dropdown li, #tab-controlls li', function(e) {
-			$('#tab-dropdown li, #tab-controlls li')
-				.removeClass('active')
-				.find('.tab')
-				.removeAttr('aria-expanded');
+		};
 
-			let text = $(this)
-				.find('.tab')
-				.text();
+	$(function () {
+		var text = $('#tab-controlls > li.active').text(),
+			$window = $(window),
+			size = 1200,
+			$sliderRootElement = $('#myCarousel'),
+			$cards = $('.card'),
+			$grid = $('.grid'),
+			$tab;
 
-			let $tab = $('.tab:contains("' + text + '")');
+		$('#tab-dropdown-label').text(text);
+
+		if ($window.width() <= size) {
+			$cards.toggleClass('card static-card');
+			$tab = $('.tab:contains("' + text + '")');
 
 			$tab
 				.attr('aria-expanded', true)
 				.parent()
 				.addClass('active');
+		}
 
-			$('#tab-dropdown-label').text(text);
-
-			$('#tab-dropdown').removeClass('open');
-		})
-		.on('click touch', '#gallery-show-more, #top-show-more', function(e) {
-			e.stopPropagation();
-			e.preventDefault();
-			let selector = $(this).attr('aria-controls');
-			// $(selector).slideDown();
-			$(selector).addClass('more');
-			$(this).addClass('hide');
-		})
-		.on('click touch', '.btn-like:enabled', function(e) {
-			e.stopPropagation();
-			e.preventDefault();
-			let count = $(this).parent('.like').find('.count');
-			count.text(+count.text() + 1);
-			$(this).attr('disabled', true);
-		})
-		.on('click touch', '.tab', function(e) {
-			setTimeout(function() {
-				$grid.masonry('layout');
-			}, 0);
-		})
-		.on('click touch', '#tab-dropdown .tab', function(e) {
-			let text = $(this).text();
-			$('#tab-dropdown-label').text(text);
-		});
+		initHeaderSlider($sliderRootElement);
+		initCardFlipper($cards);
+		initGalleryGrid($grid);
+		setEventListeners($(document), $grid);
+		windowResizeAction($window, size, $cards);
+	});
 }($));
