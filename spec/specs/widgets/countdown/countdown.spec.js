16fc9df486cf0d29876d1e814a6d48f1b27f380c
pashaOliinyk <pashades@gmail.com>
2017-02-24 11:00:21 +0200

    add: open graph meta tegs add: CDW test

--- spec/specs/widgets/countdown/countdown.spec.js
+++ spec/specs/widgets/countdown/countdown.spec.js
@@ -1,33 +1,28 @@
-// import module from '../../../../products-and-docs/widgets/countdown/1.0.0/src/main-widget.es6';
+import module from '../../../../products-and-docs/widgets/countdown/1.0.0/src/main-widget.es6';
 let  Clock = module.CountdownClock,
 	CDWidget = module.TicketmasterCountdownWidget;
 
 describe("CDWidget", () => {
-	let cdwidget ={},
-		widgetRoot ;
+	let cdwidget = CDWidget.prototype,
+	 fetchedBar;
 
-	// beforeEach(function(){
-	// 	$('<div w-type="countdown" w-type="countdown" id="toddler"></div>').appendTo('body');
-	// 	widgetRoot = document.querySelectorAll("div[w-type='countdown']");
-	// 	cdwidget = new CDWidget($("div[w-type='countdown']")[0]);
-	// 	// let mockData = {};
-	// });
-	//
-	// afterEach(function(){
-	// 	$('#toddler').remove()
-	// });
-	//
-	// it('Should be object', () => {
-	// 	expect(widgetRoot.length).toBeDefined();
-	// 	expect(typeof cdwidget).toBe('object');
-	// });
-	//
-	// it('Must have version label', () => {
-	// 	expect(widgetRoot).not.toBe(null);
-	//
-	// 	let versionEl = document.querySelectorAll(".tooltip-version")[0];
-	// 	expect(cdwidget.widgetVersion).toBeDefined();
-	// 	expect(versionEl).toBeDefined();
-	// });
+	beforeEach(function() {
+		spyOn(cdwidget,'isUniverseUrl');
+		cdwidget.isUniverseUrl("http://www.ticketmaster.com/event/");
+		fetchedBar = cdwidget.isUniverseUrl("http://www.ticketmaster.com/event/");
+	});
 
+	it('Should be object', () => {
+		expect(cdwidget.widgetVersion).toBeDefined();
+		expect(typeof cdwidget).toBe('object');
+	});
+
+	it('Should have version label', () => {
+		expect(cdwidget.widgetVersion).toBeDefined();
+		expect(typeof cdwidget.widgetVersion).toBe('string');
+	});
+
+	it('Should called "isUniverseUrl"', () => {
+		expect(cdwidget.isUniverseUrl).toHaveBeenCalledWith("http://www.ticketmaster.com/event/");
+	});
 });
