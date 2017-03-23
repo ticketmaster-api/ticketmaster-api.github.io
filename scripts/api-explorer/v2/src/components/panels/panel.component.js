6da346eff65354e7b6693c579919f36e954dc6b0
Sviatoslav Mysachenko <Sviatoslav_Mysachenko@epam.com>
2017-03-23 13:10:44 +0200

    TKMDPA-5490 [API Explorer V2][IE] Is not displayed - FIXED

--- scripts/api-explorer/v2/src/components/panels/panel.component.js
+++ scripts/api-explorer/v2/src/components/panels/panel.component.js
@@ -74,12 +74,16 @@ class Panel {
 	static isExpanded(config) {
 		return !(Object.getProp(config, '._CONFIG.collapsed') || false);
 	}
+
+	get cssClasses() {
+		return {[this.colorClass]: true, active: this.isActive};
+	}
 }
 
 module.exports = ko.components.register('panel', {
 	viewModel: Panel,
 	template:`
-		<section data-bind="css: {[colorClass]: true, active: isActive}" class="panel panel-primary">
+		<section data-bind="css: cssClasses" class="panel panel-primary">
 			<!--panel-heading-->
 			<panel-heading params="
 				showMapPopup: showMapPopup,
