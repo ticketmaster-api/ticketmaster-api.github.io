7921ea15777cb8be02566eff5b7a5337743b39c2
Sviatoslav Mysachenko <Sviatoslav_Mysachenko@epam.com>
2017-03-31 14:44:00 +0300

    TKMDPA-5674 API explorer 2: Multiple Swagger files support: added example api 'multiple-test-api' for testing swagger multiply files

--- scripts/api-explorer/v2/api.sources.js
+++ scripts/api-explorer/v2/api.sources.js
@@ -2,5 +2,9 @@ export default {
   "Discovery API v2" : {
 		api: require("_data/orgs/discovery-api/v2/api.json"),
 		meta: require("_data/orgs/discovery-api/v2/methods-metadata.json")
+	},
+	"Multiple Method Test" : {
+		api: require("_data/orgs/multiple-test-api/v2/api.json"),
+		meta: require("_data/orgs/multiple-test-api/v2/methods-metadata.json")
 	}
 }
