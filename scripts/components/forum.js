fce46c5a84d20a32d2d33888bae3e69941a43a2e
Oleksandr Kovalov <oleksandr_kovalov@epam.com>
2017-04-10 18:23:15 +0300

    pagind-add

--- scripts/components/forum.js
+++ scripts/components/forum.js
@@ -29,60 +29,70 @@ function formatDate(date) {
 	return monthNames[monthIndex] + ' ' + day + ', ' + year;
 }
 
-function getThreads(cursor) {
+function dynamicSort(property) {
+	var sortOrder = 1;
+	if(property[0] === "-") {
+		sortOrder = -1;
+		property = property.substr(1);
+	}
+	return function (a,b) {
+		var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+		return result * sortOrder;
+	}
+}
+
+var arThreads = [];
+
+function getThreads(cursor='') {
 	var apiKey ='ohRvg9N3dieJJRshzJsaQbKJAO2ZNuAebul7MeXvICNIjNKjhuhPyTfSVzrP8y8M';
-	var avatarUrl = '';
-	var profileUrl = '';
-	var userName = '';
-	var content = '';
-	var link = '';
 	$.ajax({
-		url: 'https://disqus.com/api/3.0/forums/listThreads.json?&api_key=' + apiKey + '&forum=ticketmasterapi&cursor=' + cursor,
+		url: 'https://disqus.com/api/3.0/forums/listThreads.json?&api_key=' + apiKey + '&forum=ticketmasterapi&cursor=' + cursor + '&limit=100',
 		method: 'GET',
 		dataType: 'json',
-		async: false,
 		success: function(data){
 			for (i=0;i<data.response.length;i++) {
 				if (data.response[i].posts > 0) {
-
-					$.ajax({
-						url: 'https://disqus.com/api/3.0/users/details.json?api_key=' + apiKey + '&user=' + data.response[i].author,
-						method: 'GET',
-						dataType: 'json',
-						async: false,
-						success: function (dataUser) {
-							avatarUrl = dataUser.response.avatar.cache;
-							profileUrl = dataUser.response.profileUrl;
-							userName = dataUser.response.username;
-						}
-					});
-
-					link = data.response[i].link.replace(':', '%3A');
-					link = link.replace('/', '%2F');
-
-					content += '<div class="row-forum">';
-					content += '<div class="avatar"><a href="' + profileUrl + '" title="' + userName + '" title=""><img src="' + avatarUrl + '" alt="' + userName + '" title="' + userName + '" /></a></div>';
-					content += '<div class="title"><a href="/support/forum/?category=' + data.response[i].identifiers[0] + '&link=' + data.response[i].link + '">' + data.response[i].identifiers[0] + '</a></div>';
-					content += '<div class="created">' + formatDate(data.response[i].createdAt) + '</div>';
-					content += '<div class="updated">' + formatDate(data.response[i].createdAt) + '</div>';
-					content += '<div class="posts">' + data.response[i].posts + '</div>';
-					content += '<div class="likes">' + data.response[i].dislikes + '</div>';
-					content += '</div>';
+					arThreads.push(data.response[i]);
 				}
 			}
-			if (data.cursor.hasNext==true) {
+			if (data.cursor.hasNext == true) {
 				getThreads(data.cursor.next);
 			}
-			$('#disqus-table').append(content);
 		}
 	});
+}
 
+function drawThreads(threads, start, limit) {
+	var content = '';
+	var countPages = Math.ceil(parseFloat(threads.length / limit));
+	var startPage = parseInt(start);
+	start = parseInt(start*limit);
+	limit = (parseInt(start+limit) >  parseInt(threads.length)) ? parseInt(threads.length) : parseInt(start+limit);
+
+	console.log(' LIMIT ' + start + ', ' + limit);
+	for (i = start; i < limit; i++) {
+		content += '<div class="row-forum">';
+		content += '<div class="title"><a href="/support/forum/?category=' + threads[i].identifiers[0] + '&link=' + threads[i].link + '">' + decodeURI(threads[i].clean_title) + '</a></div>';
+		content += '<div class="created">' + formatDate(threads[i].createdAt) + '</div>';
+		content += '<div class="updated">' + formatDate(threads[i].createdAt) + '</div>';
+		content += '<div class="posts">' + threads[i].posts + '</div>';
+		content += '<div class="likes">' + threads[i].dislikes + '</div>';
+		content += '</div>';
+	}
+	content += '<div class="pager">';
+	for( i = 0; i < countPages; i++) {
+		if (i != startPage) content += '<a href="/support/forum/?page=' + (i+1) + '" rel="' + i + '">' + (i+1) + '</a>';
+		else content += '<span>' + (i+1) + '</span>';
+	}
+	content += '</div>';
+	$('#disqus-table').html(content);
 }
 
 function getPosts(newIdentifier, newUrl) {
 	$('.thread .search').remove();
 	$('.thread .tabs-forum').remove();
 	$('.thread .row-forum.header').remove();
+	$('#disqus-table').remove();
   $('#disqus_header').html(decodeURI(newIdentifier));
 };
 
@@ -100,15 +110,44 @@ var disqus_config = function () {
 	(d.head || d.body).appendChild(s);
 })();
 
-$( document ).ready(function() {
+
+$(document).ready(function() {
 
 	if (!parseGetParams().hasOwnProperty('category')) {
 		$('#disqus_header').remove();
 		$('#disqus_thread').remove();
-		getThreads('');
+		getThreads();
+		$(document).ajaxStop(function() {
+			console.log(arThreads);
+			drawThreads(arThreads, 0, 8);
+		});
 	}
 	else {
 		getPosts(parseGetParams().category, parseGetParams().link);
 	}
+
+	$('body').on('click', '#disqus-table .pager a', function(e) {
+		e.preventDefault();
+		drawThreads(arThreads, parseInt($(this).attr('rel')), 8);
+	});
+
+	$('body').on('click', '.row-forum.header .title', function(e) {
+		e.preventDefault();
+		arThreads.sort(dynamicSort("clean_title"));
+		drawThreads(arThreads, 0, 8);
+	});
+
+	$('body').on('click', '.row-forum.header .created', function(e) {
+		e.preventDefault();
+		arThreads.sort(dynamicSort("createdAt"));
+		drawThreads(arThreads, 0, 8);
+	});
+
+	$('body').on('click', '.row-forum.header .posts', function(e) {
+		e.preventDefault();
+		arThreads.sort(dynamicSort("posts"));
+		drawThreads(arThreads, 0, 8);
+	});
+
 
 });
