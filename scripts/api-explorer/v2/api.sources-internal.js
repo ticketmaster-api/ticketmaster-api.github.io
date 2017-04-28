95af9d36128a5307ad7a4eda0029efd44e78dbc6
pashaOliinyk <pashades@gmail.com>
2017-04-28 12:13:11 +0300

    rem: test content for S&D items, internal/external Swagger API , multiple Swagger files, Certified partners

--- scripts/api-explorer/v2/api.sources-internal.js
+++ scripts/api-explorer/v2/api.sources-internal.js
@@ -2,13 +2,5 @@ export default {
 	"Discovery API v2" : {
 		api: require("_data/orgs/discovery-api/v2/api.json"),
 		meta: require("_data/orgs/discovery-api/v2/methods-metadata.json")
-	},
-	"Internal Commerce Swagger" : {
-		api: require("_data/orgs/commerce-swagger/v1-internal/api.json"),
-		meta: require("_data/orgs/commerce-swagger/v1-internal/methods-metadata.json")
-	},
-	"Multiple Method Test" : {
-		api: require("_data/orgs/multiple-test-api/v2/api.json"),
-		meta: require("_data/orgs/multiple-test-api/v2/methods-metadata.json")
 	}
 }
--- scripts/api-explorer/v2/api.sources.js
+++ scripts/api-explorer/v2/api.sources.js
@@ -2,13 +2,5 @@ export default {
   "Discovery API v2" : {
 		api: require("_data/orgs/discovery-api/v2/api.json"),
 		meta: require("_data/orgs/discovery-api/v2/methods-metadata.json")
-	},
-	"Commerce Swagger" : {
-		api: require("_data/orgs/commerce-swagger/v1/api.json"),
-		meta: require("_data/orgs/commerce-swagger/v1/methods-metadata.json")
-	},
-	"Multiple Method Test" : {
-		api: require("_data/orgs/multiple-test-api/v2/api.json"),
-		meta: require("_data/orgs/multiple-test-api/v2/methods-metadata.json")
 	}
 }
