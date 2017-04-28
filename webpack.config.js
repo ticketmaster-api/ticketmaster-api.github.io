f2dc4d8a2e63c1208c6562515ffd9e36890ee2e9
Oleksandr Reshetnik <oleksandr_reshetnik@epam.com>
2017-04-28 14:05:19 +0300

    reverted change in API explorer config

--- webpack.config.js
+++ webpack.config.js
@@ -10,71 +10,71 @@ var ENV = process.env.npm_lifecycle_event;
 var isProd = ENV === 'build';
 
 var config = {
-	entry: {
-		script: [
-			'babel-polyfill',
-			'./scripts/api-explorer/v2/src/main.es6.js'
-		]
-	},
-	output: {
-		path: path.join(__dirname, './scripts/api-explorer/v2/'),
-		filename: '[name].js',
-		library: "base" // global variable
-	},
-	devtool: isProd ? 'none' : 'inline-source-map',
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				include: [
-					path.resolve(__dirname, "scripts"),
-				],
-				query: {
-					presets: [
-						"es2015",
-						"stage-0"
+		entry: {
+			script: [
+				'babel-polyfill',
+				'./scripts/api-explorer/v2/src/main.es6.js'
+			]
+		},
+		output: {
+			path: path.join(__dirname, './scripts/api-explorer/v2/'),
+			filename: '[name].js',
+			library: "base" // global variable
+		},
+		devtool: isProd ? 'none' : 'inline-source-map',
+		module: {
+			loaders: [
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
+					include: [
+						path.resolve(__dirname, "scripts"),
 					],
-					plugins: [],
-					cacheDirectory: true
+					query: {
+						presets: [
+							"es2015",
+							"stage-0"
+						],
+						plugins: [],
+						cacheDirectory: true
+					}
+				},
+				{
+					test: /\.worker\.js$/,
+					loader: "worker-loader",
+					query: {
+						inline: true,
+						name: "[name].js"
+					}
+				},
+				{
+					test: /\.json/,
+					loader: "json-loader"
+				},
+				{
+					test: /\.html$/,
+					loader: 'raw'
 				}
-			},
-			{
-				test: /\.worker\.js$/,
-				loader: "worker-loader",
-				query: {
-					inline: true,
-					name: "[name].js"
-				}
-			},
-			{
-				test: /\.json/,
-				loader: "json-loader"
-			},
-			{
-				test: /\.html$/,
-				loader: 'raw'
-			}
+			],
+			noParse: /jquery[\-.0-9a-z]*/
+		},
+		plugins: [
+			new webpack.ProvidePlugin({
+				$: 'jquery',
+				jQuery: 'jquery',
+				ko: 'expose-loader?ko!knockout',
+				Clipboard: 'expose-loader?Clipboard!clipboard'
+			}),
 		],
-		noParse: /jquery[\-.0-9a-z]*/
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			$: 'jquery',
-			jQuery: 'jquery',
-			ko: 'expose-loader?ko!knockout',
-			Clipboard: 'expose-loader?Clipboard!clipboard'
-		}),
-	],
-	resolve: {
-		modules: [
-			path.resolve(__dirname),
-			path.join(__dirname, 'scripts/vendors/'),
-			path.join(__dirname, 'node_modules/'),
-			path.join(__dirname, 'scripts/components/')
-		]
-	}
-};
+		resolve: {
+			modules: [
+				path.resolve(__dirname),
+				path.join(__dirname, 'scripts/vendors/'),
+				path.join(__dirname, 'node_modules/'),
+				path.join(__dirname, 'scripts/components/')
+			]
+		}
+	};
 
 
 if (isProd) {
@@ -94,3 +94,4 @@ if (isProd) {
 }
 
 module.exports = config;
+
