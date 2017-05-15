2bdeb610c18774834fee15101d07501e66a9d982
pashaOliinyk <pashades@gmail.com>
2017-05-15 12:19:33 +0300

    mod: regex

--- scripts/components/simple-form-checker.js
+++ scripts/components/simple-form-checker.js
@@ -3,10 +3,10 @@ var simpleFormService = {};
 
 (function () {
 	simpleFormService.checkKey = function (formKey, formKeyCC) {
-		var localhost = /(localhost:4000|127\.0\.0\.1)+/ig,
-			staging = 'ticketmaster-api-staging.github.io',
+		var localhost = /(localhost:4000|127\.0\.0\.1|ticketmaster-api-staging.github.io)+/ig,
+			//staging = 'ticketmaster-api-staging.github.io',
 			host = window.location.host;
-		if (localhost.test(host) || staging.test(host)) {
+		if (localhost.test(host) ) {
 			formKey = '3d9f2df7bef3e8bc5d9323cbea36f4d0'; 
 			formKeyCC = '3d9f2df7bef3e8bc5d9323cbea36f4d0';
 		}
