d1ca7b174c5f1d6b8e3e826afa545393b18bac38
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-06-13 13:56:13 +0300

    Added report Portal to framework

--- tests/serenity/src/test/java/com/tkmdpa/taf/AcceptanceTestSuite.java
+++ tests/serenity/src/test/java/com/tkmdpa/taf/AcceptanceTestSuite.java
@@ -1,7 +1,19 @@
 package com.tkmdpa.taf;
 
+import com.epam.reportportal.jbehave.ReportPortalFormat;
+import com.epam.reportportal.jbehave.ReportPortalViewGenerator;
 import net.serenitybdd.jbehave.SerenityStories;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.reporters.StoryReporterBuilder;
 
 public class AcceptanceTestSuite extends SerenityStories {
     public static String baseTestedUrl = new SerenityStories().getEnvironmentVariables().getProperty("webdriver.base.url");
+
+
+    public AcceptanceTestSuite() {
+        new MostUsefulConfiguration()
+                .useStoryReporterBuilder(new StoryReporterBuilder()
+                        .withFormats(ReportPortalFormat.INSTANCE))
+                .useViewGenerator(new ReportPortalViewGenerator());
+    }
 }
